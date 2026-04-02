c3c9c88488747bf1f1eba9b39edfd6580a83b3ad
TIGREAL0 <gorakshvnistane@gmail.com>
2026-04-02 14:20:49 +0530

    Update script1_system_identity.sh

--- script1_system_identity.sh
+++ script1_system_identity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 1: System Identity Report
-# Author: Ayush Pandey | Reg No: 24BCE11356
+# Author: Goraksh Mistane | Reg No: 24BCE10939
 # Course: Open Source Software | OSS NGMC Capstone Project
 # Description: Displays a welcome screen with system info
 #              and open-source license details for the OS.
